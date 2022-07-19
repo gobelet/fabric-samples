1357ed94fee5fa52b16f6210e021ebdb152dc1e1
gobelet <36410824+gobelet@users.noreply.github.com>
2022-07-19 18:03:45 +0900

    Add files via upload

---
+++ asset-transfer-basic/application-javascript/enterQueue.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const { Gateway, Wallets } = require('fabric-network');
+const path = require('path');
+const { buildCCPOrg1} = require('../../test-application/javascript/AppUtil.js');
+
+const channelName = 'mychannel';
+const chaincodeName = 'basic';
+const walletPath = path.join(__dirname, 'wallet');
+const org1UserId = 'appUser';
+
+async function main() {
+	try {
+		// build an in memory object with the network configuration (also known as a connection profile)
+		const ccp = buildCCPOrg1();
+
+		// setup the wallet to hold the credentials of the application user
+		const wallet = await Wallets.newFileSystemWallet(walletPath);
+
+		// Create a new gateway instance for interacting with the fabric network.
+		// In a real application this would be done as the backend server session is setup for
+		// a user that has been verified.
+		const gateway = new Gateway();
+
+		try {
+			// setup the gateway instance
+			// The user will now be able to create connections to the fabric network and be able to
+			// submit transactions and query. All transactions submitted by this gateway will be
+			// signed by this user using the credentials stored in the wallet.
+			await gateway.connect(ccp, {
+				wallet,
+				identity: org1UserId,
+				discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
+			});
+
+			// Build a network instance based on the channel where the smart contract is deployed
+			const network = await gateway.getNetwork(channelName);
+
+			// Get the contract from the network.
+			const contract = network.getContract(chaincodeName);
+
+			const clientId = process.argv[2];
+			let assetId = process.argv[3];
+			if (assetId === undefined) {
+				assetId = 'queue1';
+			}
+
+			console.log('\n--> Submit Transaction: EnterQueue, enters queue with clientId');
+			await contract.submitTransaction('EnterQueue', assetId, clientId);
+			console.log('*** Result: committed');
+
+		} finally {
+			// Disconnect from the gateway when the application is closing
+			// This will close all connections to the network
+			gateway.disconnect();
+		}
+	} catch (error) {
+		console.error(`******** FAILED to run the application: ${error}`);
+	}
+}
+
+main();
---
+++ asset-transfer-basic/application-javascript/readQueue.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const { Gateway, Wallets } = require('fabric-network');
+const path = require('path');
+const { buildCCPOrg1} = require('../../test-application/javascript/AppUtil.js');
+
+const channelName = 'mychannel';
+const chaincodeName = 'basic';
+const walletPath = path.join(__dirname, 'wallet');
+const org1UserId = 'appUser';
+
+function prettyJSONString(inputString) {
+	return JSON.stringify(JSON.parse(inputString), null, 2);
+}
+
+async function main() {
+	try {
+		// build an in memory object with the network configuration (also known as a connection profile)
+		const ccp = buildCCPOrg1();
+
+		// read the wallet to hold the credentials of the application user
+		const wallet = await Wallets.newFileSystemWallet(walletPath);
+
+		// Create a new gateway instance for interacting with the fabric network.
+		// In a real application this would be done as the backend server session is setup for
+		// a user that has been verified.
+		const gateway = new Gateway();
+
+		try {
+			// setup the gateway instance
+			// The user will now be able to create connections to the fabric network and be able to
+			// submit transactions and query. All transactions submitted by this gateway will be
+			// signed by this user using the credentials stored in the wallet.
+			await gateway.connect(ccp, {
+				wallet,
+				identity: org1UserId,
+				discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
+			});
+
+			// Build a network instance based on the channel where the smart contract is deployed
+			const network = await gateway.getNetwork(channelName);
+
+			// Get the contract from the network.
+			const contract = network.getContract(chaincodeName);
+
+			const assetId = process.argv[2];
+
+			//console.log('\n--> Evaluate Transaction: ReadStatus, function returns rank of clientId');
+			console.log('\n--> Evaluate Transaction: ReadAsset, function returns assetId attributes');
+			let result = await contract.evaluateTransaction('ReadAsset', assetId);
+			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+
+		} finally {
+			// Disconnect from the gateway when the application is closing
+			// This will close all connections to the network
+			gateway.disconnect();
+		}
+	} catch (error) {
+		console.error(`******** FAILED to run the application: ${error}`);
+	}
+}
+
+main();
